10e80ef0d185a162beb0f786a867ec7493d80cc9
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2022-01-09 17:34:26 -0500

    implemented os modules

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const names = require("./4-names.js");
-const sayHi = require("./5-utils.js");
-const data = require("./6-alternative-flavor.js");
-require("./7-mind-grenade.js");
+const os = require("os");
 
-// sayHi("susan");
-// sayHi(names.john);
-// sayHi(names.peter);
+// info about current user
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime in seconds
+console.log(`the system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+console.log(currentOS);
