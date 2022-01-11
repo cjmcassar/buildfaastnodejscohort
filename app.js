c4fc71ad9b2ee371db023c725df512ef9ebb8b34
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2022-01-10 23:12:34 -0500

    tested out fs module async

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const os = require("os");
+const { readFile, writeFile } = require("fs");
 
-// info about current user
-const user = os.userInfo();
-console.log(user);
-
-// method returns the system uptime in seconds
-console.log(`the system uptime is ${os.uptime()} seconds`);
-
-const currentOS = {
-  name: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-};
-
-console.log(currentOS);
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    const first = result;
+    readFile("./content/second.txt", "utf8", (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const second = result;
+      writeFile(
+        "./content/result-async.txt",
+        `here is the result: ${first}, ${second}`,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log(result);
+        }
+      );
+    });
+  }
+});
