44bbd1c359a23a5fe5c9f7d75467e43182c12916
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-01-19 04:21:15 +0000

    Created file for http test

---
+++ 12-http.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+	if (req.url === "/") {
+		res.end("What's up cohort?");
+	}
+	if (req.url === "/about") {
+		res.end("This is the about page");
+	}
+	res.end("404");
+});
+
+server.listen(3000);
